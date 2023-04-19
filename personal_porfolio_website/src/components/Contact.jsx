5974e42dc05ce3998537501003d244a9ee26c5dd
Jeison Sanchez <58638129+46lemonlime@users.noreply.github.com>
2023-04-19 08:22:32 +0100

    debug
    - fixed navbar bug

--- personal_porfolio_website/src/components/Contact.jsx
+++ personal_porfolio_website/src/components/Contact.jsx
@@ -4,6 +4,7 @@ import styles from "../style";
 const Contact = () => {
   return (
     <section
+      id="contact"
       className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
     >
       <div className="flex-1 flex flex-col">
